b4b337853066848ab4c160210c441839f96e1dee
jschless <joe.c.schlessinger@gmail.com>
2024-07-30 20:25:34 -0400

    Initial commit

---
+++ fit_model.py
@@ -0,0 +1,153 @@
+import os
+import numpy as np
+import librosa
+
+from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import make_scorer, accuracy_score, classification_report
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+import joblib
+
+
+def initialize_dataset():
+    file_paths, labels = [], []
+    NOT_TRAIN_DIR = (
+        "/Users/joeschlessinger/Programming/trainTrackER/recordings/not_train"
+    )
+    TRAIN_DIR = "/Users/joeschlessinger/Programming/trainTrackER/recordings/train"
+
+    for file in [os.path.join(TRAIN_DIR, f) for f in os.listdir(TRAIN_DIR)]:
+        if file.endswith(".wav"):
+            file_paths.append(file)
+            labels.append(1)
+
+    for file in [os.path.join(NOT_TRAIN_DIR, f) for f in os.listdir(NOT_TRAIN_DIR)]:
+        if file.endswith(".wav"):
+            file_paths.append(file)
+            labels.append(0)
+
+    return file_paths, labels
+
+
+def extract_features(file_path, n_bins=10, min_freq=0, max_freq=1000):
+    # takes file and outputs X for the model
+    y, sr = librosa.load(file_path, sr=None)
+
+    stft = np.abs(librosa.stft(y))
+    freqs = librosa.fft_frequencies(sr=sr)
+    target_freqs = freqs[(freqs >= min_freq) & (freqs <= max_freq)]
+    target_stft = stft[(freqs >= min_freq) & (freqs <= max_freq)]
+    bin_edges = np.linspace(target_freqs[0], target_freqs[-1], n_bins + 1)
+    mean_features = []
+    std_features = []
+    for i in range(n_bins):
+        bin_mask = (target_freqs >= bin_edges[i]) & (target_freqs < bin_edges[i + 1])
+        bin_stft = target_stft[bin_mask]
+        mean_features.append(np.mean(bin_stft))
+        std_features.append(np.std(bin_stft))
+    features = np.hstack([mean_features, std_features])
+    return features
+
+
+class CustomFeatureExtractor(BaseEstimator, TransformerMixin):
+    def __init__(self, n_bins=10, min_freq=0, max_freq=1000):
+        self.n_bins = n_bins
+        self.min_freq = min_freq
+        self.max_freq = max_freq
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return np.array(
+            [
+                extract_features(fp, self.n_bins, self.min_freq, self.max_freq)
+                for fp in X
+            ]
+        )
+
+
+def train_model_explicit(n_bins, min_freq, max_freq, save=False, file_name=None):
+    file_paths, labels = initialize_dataset()
+
+    X = np.array(
+        [
+            extract_features(fp, n_bins=n_bins, min_freq=min_freq, max_freq=max_freq)
+            for fp in file_paths
+        ]
+    )
+    y = np.array(labels)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    clf = RandomForestClassifier(n_estimators=100, random_state=42)
+    clf.fit(X_train, y_train)
+
+    y_pred = clf.predict(X_test)
+
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+    print("Classification Report:")
+    print(classification_report(y_test, y_pred))
+
+    if save:
+        joblib.dump(clf, f"./{file_name}.pkl")
+
+
+def train_model_grid_search():
+    file_paths, labels = initialize_dataset()
+    X = np.array([extract_features(fp) for fp in file_paths])
+    y = np.array(labels)
+    print(f"Dataset initialized with {len(labels)} data points")
+    X_train_paths, X_test_paths, y_train, y_test = train_test_split(
+        file_paths, labels, test_size=0.2, random_state=42
+    )
+
+    pipe = Pipeline(
+        [
+            ("feature_extractor", CustomFeatureExtractor()),
+            ("scaler", StandardScaler()),
+            ("classifier", RandomForestClassifier(n_estimators=100, random_state=42)),
+        ]
+    )
+
+    param_grid = {
+        "feature_extractor__n_bins": list(range(5, 41, 5)),
+        "feature_extractor__min_freq": list(range(0, 501, 100)),
+        "feature_extractor__max_freq": list(range(500, 1001, 100)),
+    }
+
+    grid_search = GridSearchCV(pipe, param_grid, cv=StratifiedKFold(n_splits=5))
+
+    grid_search.fit(X_train_paths, y_train)
+
+    print("Best parameters found: ", grid_search.best_params_)
+
+    y_pred = grid_search.best_estimator_.predict(X_test_paths)
+
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+    print("Classification Report:")
+    print(classification_report(y_test, y_pred))
+
+    return grid_search.best_estimator_
+
+
+def get_best_model():
+    best_model = joblib.load("./best_model.pkl")
+
+
+def classify_audio(file_path, model):
+    return model.predict([file_path])[0]
+
+
+if __name__ == "__main__":
+    best_model = train_model_grid_search()
+
+    joblib.dump(best_model_, "./best_model.pkl")
---
+++ main.py
@@ -0,0 +1,148 @@
+import pyaudio
+import wave
+import numpy as np
+import datetime as dt
+import time
+import argparse
+
+from fit_model import extract_features
+
+
+# Parameters
+CHUNK_SIZE = 512
+FORMAT = pyaudio.paInt16
+RATE = 44100
+THRESHOLD = 2000
+LENGTH_THRESHOLD = 10  # number of above-threshold frames you must here within RECORD_DELAY window to classify as train
+RECORD_DELAY = 250  # number of sub-threshold frames to wait before saving clip
+VERBOSE = False
+
+
+def classify_audio(file_path, model):
+    features = extract_features(
+        file_path,
+        n_bins=model.named_steps["feature_extractor"].n_bins,
+        min_freq=model.named_steps["feature_extractor"].min_freq,
+        max_freq=model.named_steps["feature_extractor"].max_freq,
+    )
+    features = model.named_steps["scaler"].transform([features])
+    prediction = model.named_steps["classifier"].predict(features)
+    return prediction[0]
+
+
+def detect_train_horn(audio_data):
+    # Basic threshold-based detection
+    max_amplitude = np.max(np.abs(audio_data))
+    return max_amplitude > THRESHOLD
+
+
+def format_filename(filename):
+    # 'train_horn_20240326_115843.wav' turn this into DTG
+    dtg = filename[filename.find("train_horn_") + 11 : -4]
+    dt = datetime.strptime(dtg, "%Y%m%d_%H%M%S")
+    return dt.strftime("%a %d %b %Y, %I:%M%p")
+
+
+def record_audio(classify_and_delete=False, model_file=None):
+    model = None
+    if classify_and_delete:
+        model = joblib.load("./working_best_model.pkl")
+        print("Classifying in real time and only saving trains")
+    else:
+        print("Recording all loud sustained noises.")
+    p = pyaudio.PyAudio()
+
+    device_index = p.get_default_input_device_info()["index"]
+
+    stream = p.open(
+        format=FORMAT,
+        channels=1,
+        rate=RATE,
+        input=True,
+        frames_per_buffer=CHUNK_SIZE,
+        input_device_index=device_index,
+    )
+
+    frames = []
+    is_recording = False
+    quiet_frames = 0
+    loud_frames = 0
+    while True:
+        data = stream.read(CHUNK_SIZE, exception_on_overflow=False)
+        audio_data = np.frombuffer(data, dtype=np.int16)
+
+        if detect_train_horn(audio_data):
+            if VERBOSE:
+                print("Possible train horn detected at:", datetime.datetime.now())
+            loud_frames += 1
+            frames.append(data)
+            is_recording = True
+
+        if is_recording and not detect_train_horn(audio_data):
+            frames.append(data)
+            quiet_frames += 1
+            if quiet_frames > RECORD_DELAY:
+                if loud_frames > LENGTH_THRESHOLD:
+                    if VERBOSE:
+                        print("Possible train horn ended at:", datetime.datetime.now())
+                        print("confirmed " + str(LENGTH_THRESHOLD) + " loud frames")
+                    file_name = save_recorded_clip(frames)
+
+                    if classify_and_delete and classify_audio(file_path, model) == 1:
+                        print(f"Train detected at {format_filename(file_name)}")
+                        print(
+                            f"Train detected at {format_filename(file_name)}",
+                            file=open("./train.log"),
+                        )
+                        save_recorded_clip(frames, save_to_special_dir)
+                        os.remove(file_name)
+                    elif classify_and_delete:
+                        if VERBOSE:
+                            print("Not a train, deleting recording")
+                        os.remove(file_name)
+
+                else:
+                    if VERBOSE:
+                        print(
+                            "not identified as a train, loud_frames was ", loud_frames
+                        )
+
+                frames = []
+                is_recording = False
+                quiet_frames = 0
+                loud_frames = 0
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+
+def save_recorded_clip(frames, save_to_special_dir=False):
+    if save_to_special_dir:
+        filename = f"./recordings/classified_trains/train_horn_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.wav"
+    else:
+        filename = f"./recordings/train_horn_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.wav"
+    wf = wave.open(filename, "wb")
+    wf.setnchannels(1)
+    wf.setsampwidth(2)  # 16-bit audio
+    wf.setframerate(RATE)
+    wf.writeframes(b"".join(frames))
+    wf.close()
+    if VERBOSE:
+        print("Saved recorded clip as:", filename)
+    return filename
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process optional arguments.")
+    parser.add_argument(
+        "--classify_and_delete",
+        type=bool,
+        default=False,
+        help="If you want to real time process noises and only save trains",
+    )
+    parser.add_argument("--model", type=str, help="Path to model", default=None)
+
+    args = parser.parse_args()
+
+    record_audio(args.classify_and_delete, args.model)
